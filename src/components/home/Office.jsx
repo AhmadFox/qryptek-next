9b95faa65578c24b4b04c9433ee983b33b89069a
Ahmad Issa Gharaibeh <GHARAIBEH.AHMAD88@GMAIL.COM>
2023-08-12 15:46:15 +0300

    fix map image

--- src/components/home/Office.jsx
+++ src/components/home/Office.jsx
@@ -19,7 +19,7 @@ const Office = () => {
 			<Container>
 				<div className="relative">
 					<Image
-						className="h-auto w-full max-w-max mx-auto -translate-x-10"
+						className="h-auto w-full max-w-max mx-auto lg:-translate-x-10"
 						src="/world_map.svg"
 						alt="World Map"
 						width={988}
@@ -29,13 +29,13 @@ const Office = () => {
 						priority
 					/>
 				</div>
-				<Title type='p' display='d6' className='text-center my-12'>QrypteK Global Offices</Title>
+				<Title type='p' display='d6' className='text-center !text-sm sm:!text-lg !md:text-xl !lg:text-2xl my-12'>QrypteK Global Offices</Title>
 
-				<div className="w-1/2 mx-auto text-center mt-24 relative">
+				<div className="md:w-1/2 mx-auto text-center xl:mt-24 relative">
 			
-					<Title type='p' display='d1'>Ready for The Next Step?</Title>
+					<Title type='p' display='d1' className=''>Ready for The Next Step?</Title>
 
-					<Button onClick={openRequsetModal} fill={'true'} rounded={'true'} className={'flex md:shrink-0 flex-col justify-center items-center p-3 xl:p-5 w-24 sm:w-32 md:w-36 xl:w-44 h-24 sm:h-32 md:h-36 xl:h-44 ml-auto translate-x-16 -translate-y-24 -mb-24'}>
+					<Button onClick={openRequsetModal} fill={'true'} rounded={'true'} className={'flex md:shrink-0 flex-col justify-center items-center p-3 xl:p-5 w-24 sm:w-32 md:w-36 xl:w-44 h-24 sm:h-32 md:h-36 xl:h-44 mx-auto lg:mr-0 lg:translate-x-20 xl:translate-x-16 lg:-translate-y-20 2xl:-translate-y-28 mt-6 lg:mt-0 lg:-mb-24'}>
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.75} stroke="currentColor" className="w-4 sm:w-6 h-4 sm:h-6 mx-auto mb-2">
 							<path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
 						</svg>
